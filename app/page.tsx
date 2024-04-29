850b78300238159b639c46b56e8938cf5203ad33
Timothy Kunke <tkunke@Timothys-MBP.hsd1.ca.comcast.net>
2024-04-29 11:41:25 -0700

    updated banner

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="m-4">
           <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">Cinetech Assistant</h1>
           <div className="mb-6 text-normal font-normal text-gray-500">
-            This is an AI-powered technical assistant.
+            This the beta version of our new technical assistant powered by OpenAI.
           </div>
         </div>
         <CinetechAssistant 
