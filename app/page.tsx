bfb0a79f1b0aab14f1304e44b30de92becc3afc4
Timothy Kunke <tkunke@Timothys-MBP.hsd1.ca.comcast.net>
2024-05-01 10:19:23 -0700

    updated home page with new logo and repositioned text

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       {/* Image container */}
       <div className="relative w-full overflow-hidden">
         <Image
-          src="/Cinetech_Webste_Logo_Background0001.png"
+          src="/Cinetech_Logo_Clean_Blue_001.png"
           width={1920}
           height={1080}
           alt="Background"
@@ -16,33 +16,31 @@ export default function Home() {
       </div>
       
       {/* Text overlay */}
-      <div className="absolute top-1/3 left-0 w-full text-white">
-        <div className="container mx-auto px-4 py-12">
+      <div className="absolute bottom-0 left-0 w-full h-full text-white">
+        <div className="container mx-auto px-4 py-12 h-full flex flex-col justify-end">
           {/* Text above the button */}
-          <div className="mb-8">
+          <div>
             <h1 className="text-7xl font-bold mb-4">
               Welcome to the CineTech Assistant
             </h1>
-            <p className="text-2xl">
+            <p className="text-2xl p-8">
               A Technical Assistant Using OpenAI
             </p>
+            <p className="text-xl mb-4">
+              The CineTech Assistant is designed as an advanced virtual aide for
+              filmmakers, cinematographers, and enthusiasts engaged in the cinematic arts and technologies.
+              It is specifically designed to serve a wide spectrum of needs in the film and video production domains.
+            </p>
+            <Link href="/assistant">
+              <button className="px-12 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-700 transition">
+                Try the Assistant
+              </button>
+            </Link>
           </div>
         </div>
-      </div>
-
-      {/* Description and button container */}
-      <div className="container mx-auto px-4 py-12 text-center">
-        <p className="text-xl mb-4">
-          The CineTech Assistant is designed as an advanced virtual aide for
-          filmmakers, cinematographers, and enthusiasts engaged in the cinematic arts and technologies.
-          It is tailored to serve a wide spectrum of needs in the film and video production domains.
-        </p>
-        <Link href="/assistant">
-          <button className="px-12 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-700 transition">
-            Try the Assistant
-          </button>
-        </Link>
       </div>
     </div>
   );
 }
+
+
