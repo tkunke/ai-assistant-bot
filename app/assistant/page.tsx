d5984c298103f9867c79ffc5bd3b6b3fe998a074
Timothy Kunke <tkunke@Timothys-MBP.hsd1.ca.comcast.net>
2024-04-30 21:30:14 -0700

    updated message box transparency and background image to scale with user scrolls

--- app/assistant/page.tsx
+++ app/assistant/page.tsx
@@ -5,11 +5,10 @@ import '/app/globals.css';
 export default function Home() {
   return (
     <main>
-      <div className="page-background h-screen">
       <div className="mx-auto mb-12 max-w-custom text-center p-8 rounded-lg">
         <div className="m-4">
           <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">CineTech Assistant</h1>
-          <div className="mb-6 text-normal font-normal text-gray-500">
+          <div className="mb-6 text-normal font-normal text-gray-800">
             This the beta version of our new technical assistant powered by OpenAI.
           </div>
         </div>
@@ -18,7 +17,6 @@ export default function Home() {
           greeting="Hey there! How can I help?"
           messageLimit={10}
         />
-      </div>
       </div>
     </main>
   );
--- app/ui/cinetech-assistant.jsx
+++ app/ui/cinetech-assistant.jsx
@@ -180,7 +180,7 @@ export function CinetechAssistantMessage({ message }) {
     const backgroundColor = message.role === 'assistant' ? 'bg-gray-200' : 'bg-white';
 
     return (
-        <div className={`flex flex-col rounded text-gray-700 text-center ${backgroundColor} px-4 py-2 m-2 shadow-md`} style={{ alignItems: 'flex-start' }}>
+        <div className={`flex flex-col rounded text-gray-700 text-center ${backgroundColor} bg-opacity-20 px-4 py-2 m-2 shadow-md`} style={{ alignItems: 'flex-start' }}>
             <div className="text-4xl">
                 {displayRole(message.role)}
             </div>
