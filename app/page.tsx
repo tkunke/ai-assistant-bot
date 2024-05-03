7dd821db0007b2428c2a07aeb556382289a9d536
Timothy Kunke <tkunke@Timothys-MBP.hsd1.ca.comcast.net>
2024-05-02 17:47:48 -0700

    adjusted class for responsiveness and removed comments

--- app/page.tsx
+++ app/page.tsx
@@ -17,23 +17,23 @@ export default function Home() {
       </div>
       
       {/* Text overlay */}
-      <div className="absolute bottom-0 left-0 w-full h-full text-white">
+      <div className="absolute bottom-0 left-0 w-full h-50vh text-white">
         <div className="container mx-auto px-4 py-12 h-full flex flex-col justify-end">
           {/* Text above the button */}
           <div>
-            <h1 className="text-4xl md:text-7xl font-bold mb-4"> {/* Adjusted text size */}
+            <h1 className="text-4xl md:text-7xl font-bold mb-4">
               Welcome to the CineTech Assistant
             </h1>
-            <p className="text-lg md:text-2xl p-4 md:p-8"> {/* Adjusted text size */}
+            <p className="text-lg md:text-2xl p-4 md:p-8">
               A Technical Assistant Using OpenAI
             </p>
-            <p className="text-base md:text-xl mb-4"> {/* Adjusted text size */}
+            <p className="text-base md:text-xl mb-4">
               The CineTech Assistant is designed as an advanced virtual aide for
               filmmakers, cinematographers, and enthusiasts engaged in the cinematic arts and technologies.
               It is specifically designed to serve a wide spectrum of needs in the film and video production domains.
             </p>
             <Link href="/assistant">
-              <button className="px-6 py-2 md:px-12 md:py-3 bg-blue-500 text-white rounded-full hover:bg-blue-700 transition"> {/* Adjusted button size */}
+              <button className="px-6 py-2 md:px-12 md:py-3 bg-blue-500 text-white rounded-full hover:bg-blue-700 transition">
                 Try the Assistant
               </button>
             </Link>
