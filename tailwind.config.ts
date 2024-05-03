841a4bde683651edb3213d4cec62febcfaeaa76c
Timothy Kunke <tkunke@Timothys-MBP.hsd1.ca.comcast.net>
2024-05-03 08:40:38 -0700

    added addtional breakpoints for varying screen size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        'xs': '360px', // Extra small devices (phones, 360px and down)
+        'sm': '640px', // Small devices (phones, 640px and down)
+        'md': '768px', // Medium devices (tablets, 768px and down)
+        'lg': '1024px', // Large devices (desktops, 1024px and down)
+        'xl': '1280px', // Extra large devices (large desktops, 1280px and down)
+        '2xl': '1536px', // 2xl screens (1536px and up)
+      },
     },
   },  
   plugins: [],
