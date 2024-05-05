b42a91e412b36a52d68ceaacfe847fa2b2de1559
Timothy Kunke <tkunke@Timothys-MBP.hsd1.ca.comcast.net>
2024-05-04 21:08:02 -0700

    testing new orientation logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,34 @@
-'use client';
+'use client'
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { MdHeight } from 'react-icons/md';
 import {useMediaQuery} from 'react-responsive';
 
 export default function Home() {
-  // Function to check the screen orientation
-  const getScreenOrientation = () => {
-    return window.matchMedia("(orientation: portrait)").matches ? "portrait" : "landscape";
-  };
+  const [screenOrientation, setScreenOrientation] = useState("portrait");
+
+  useEffect(() => {
+    const getScreenOrientation = () => {
+      return window.matchMedia("(orientation: portrait)").matches ? "portrait" : "landscape";
+    };
+
+    const handleOrientationChange = () => {
+      setScreenOrientation(getScreenOrientation());
+    };
+
+    setScreenOrientation(getScreenOrientation()); // Initial check
+
+    window.addEventListener("orientationchange", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("orientationchange", handleOrientationChange);
+    };
+  }, []);
 
   // Define background images for portrait and landscape orientations
   const portraitBackground = "/background_portrait.png";
   const landscapeBackground = "/background_landscape.png";
-
-  // Determine the current screen orientation
-  const screenOrientation = getScreenOrientation();
 
   // Render different background images based on the screen orientation
   return (
