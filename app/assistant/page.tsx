77f06dbe470a6f869b71ecf61cc2f13e5eab588f
Timothy Kunke <tkunke@Timothys-MBP.hsd1.ca.comcast.net>
2024-05-05 11:26:33 -0700

    updates to assistant page for orientation adjustments

--- app/assistant/page.tsx
+++ app/assistant/page.tsx
@@ -1,18 +1,44 @@
+'use client';
+import { useEffect, useState } from "react";
 import CinetechAssistant from "@/app/ui/cinetech-assistant";
 import '/app/globals.css';
 
 
 export default function Home() {
+  const [screenOrientation, setScreenOrientation] = useState("portrait");
+
+  useEffect(() => {
+    const getScreenOrientation = () => {
+      return window.matchMedia("(orientation: portrait)").matches ? "portrait" : "landscape";
+    };
+
+    const handleOrientationChange = () => {
+      setScreenOrientation(getScreenOrientation());
+    };
+
+    setScreenOrientation(getScreenOrientation()); // Initial check
+
+    window.addEventListener("orientationchange", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("orientationchange", handleOrientationChange);
+    };
+  }, []);
+
+  // Define background images for portrait and landscape orientations
+  const portraitBackground = "/background_portrait.png";
+  const landscapeBackground = "/background_landscape.png";
+
   return (
-    <div>
-      <header className="header">
+    <div className="relative text-center" style={{ width: "100vw", height: "100vh", backgroundImage: `url(${screenOrientation === 'portrait' ? portraitBackground : landscapeBackground})` }}>
+      <header className="header" style={{ backgroundColor: 'transparent' }}>
         <div className="mx-auto mb-12 max-w-custom text-center p-3 rounded-lg" style={{ height: '10vh' }}>
-            <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">CineTech Assistant</h1>
-            <p className="text-gray-800">This is the beta version of our new technical assistant powered by OpenAI.</p>
+        <h1 className="mb-4 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold leading-none tracking-tight text-gray-900">CineTech Assistant</h1>
+        <p className="text-sm sm:text-base md:text-lg text-gray-800">This is the beta version of our new technical assistant powered by OpenAI.</p>
         </div>
       </header>
       <main>
-        <div className="mx-auto mb-12 max-w-custom text-center p-8 rounded-lg">
+        <div className="mx-auto mb-12 text-sm sm:text-base md:text-lg text-center p-8 rounded-lg"> 
           <CinetechAssistant 
             assistantId="asst_fmjzsttDthGzzJud4Vv2bDGq"
             greeting="Hey there! How can I help?"
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,33 @@
-'use client'
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { MdHeight } from 'react-icons/md';
-import {useMediaQuery} from 'react-responsive';
 
 export default function Home() {
-  const [screenOrientation, setScreenOrientation] = useState("portrait");
-
-  useEffect(() => {
-    const getScreenOrientation = () => {
-      return window.matchMedia("(orientation: portrait)").matches ? "portrait" : "landscape";
-    };
-
-    const handleOrientationChange = () => {
-      setScreenOrientation(getScreenOrientation());
-    };
-
-    setScreenOrientation(getScreenOrientation()); // Initial check
-
-    window.addEventListener("orientationchange", handleOrientationChange);
-
-    return () => {
-      window.removeEventListener("orientationchange", handleOrientationChange);
-    };
-  }, []);
-
-  // Define background images for portrait and landscape orientations
-  const portraitBackground = "/background_portrait.png";
-  const landscapeBackground = "/background_landscape.png";
-
-  // Render different background images based on the screen orientation
   return (
-    <div className="relative text-center" style={{ width: "100vw", height: "100vh", backgroundImage: `url(${screenOrientation === 'portrait' ? portraitBackground : landscapeBackground})` }}>
-      {/* Add other content here */}
+    <div className="relative text-center">
+      {/* Image container */}
+      <div className="relative w-full overflow-hidden">
+        <Image
+          src="/Cinetech_Webste_Logo__Final_.png"
+          alt="background"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full h-auto"
+        />
+      </div>
+      
+      {/* Button overlay */}
+<div className="absolute bottom-20 left-0 w-full h-1/3 flex items-center justify-center">
+  <div className="container mx-auto px-4 py-6 md:py-12 flex flex-col items-center">
+    <div className="mb-6">
+      <Link href="/assistant">
+        <button className="px-6 py-2 md:px-12 md:py-3 bg-white text-gray-700 rounded-full hover:bg-gray-200 hover:text-gray-900 transition">
+          Try the Assistant
+        </button>
+      </Link>
+    </div>
+  </div>
+</div>
     </div>
   );
 }
